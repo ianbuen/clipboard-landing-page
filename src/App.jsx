1d0c20353698129912d43c67328ee891c93ad0ac
ianbuen <ianbuen@gmail.com>
2023-05-11 10:16:16 +0800

    create intro

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { Download } from './components/Download'
 import './styles/App.sass'
+import logo from "./assets/images/logo.svg"
 
 function App() {
 
   return (
     <main className='App'>
-      
+      <section className='Intro'>
+        <img src={logo} alt="logo" />
+        <h1>A history of everything you copy</h1>
+        <p>Clipboard allows you to track and organize everything you copy. Instantly access your clipboard on all you devices.</p>
+        <Download />
+      </section>
     </main>
   )
 }
---
+++ src/components/Download.jsx
@@ -0,0 +1,10 @@
+import "../styles/Download.sass"
+
+export const Download = () => {
+  return (
+    <div className="Download">
+        <button>Download for iOS</button>
+        <button>Download for Mac</button>
+    </div>
+  )
+}
