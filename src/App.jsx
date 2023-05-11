aec565435c632e1b59ae52d24ac24a84c74b9800
ianbuen <ianbuen@gmail.com>
2023-05-11 12:07:14 +0800

    add brand logos

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import { Download } from './components/Download'
-import './styles/App.sass'
-import logo from "./assets/images/logo.svg"
-import imgPC from "./assets/images/image-computer.png"
+import { Download } from './components/Download';
+import './styles/App.sass';
+import logo from "./assets/images/logo.svg";
+import imgPC from "./assets/images/image-computer.png";
+import imgGoogle from "./assets/images/logo-google.png";
+import imgIBM from "./assets/images/logo-ibm.png";
+import imgMicrosoft from "./assets/images/logo-microsoft.png";
+import imgHP from "./assets/images/logo-hp.png";
+import imgVector from "./assets/images/logo-vector-graphics.png";
 
 function App() {
 
@@ -19,39 +24,41 @@ function App() {
         <p>Clipboard instantly stores any item you copy in the cloud, meaning you can access your snippets immediately on all your devices. Our Mac and iOS apps will help you organize everything.</p>
         <img src={imgPC} alt="photo of computer" />
 
-        <h2>Quick Search</h2>
+        <h3>Quick Search</h3>
         <p>Easily search your snippets by content, category, web address, application, and more.</p>
         
 
-        iCloud Sync
+        <h3>iCloud Sync</h3>
+        <p>Instantly saves and syncs snippets across all your devices.</p>
 
-        Instantly saves and syncs snippets across all your devices.
+        <h3>Complete History</h3>
+        <p>Retrieve any snippets from the first moment you started using the app.</p>
 
-        Complete History
-
-        Retrieve any snippets from the first moment you started using the app.
-
-        Access Clipboard anywhere
-
-        Whether you’re on the go, or at your computer, you can access all your Clipboard 
-        snippets in a few simple clicks.
-
-        Supercharge your workflow
-
-        We’ve got the tools to boost your productivity.
-
-        Create blacklists
-
-        Ensure sensitive information never makes its way to your clipboard by excluding certain sources.
-
-        Plain text snippets
-
-        Remove unwanted formatting from copied text for a consistent look.
-
-        Sneak preview
-
-        Quick preview of all snippets on your Clipboard for easy access.
         
+        <h2>Access Clipboard anywhere</h2>
+        <p>Whether you’re on the go, or at your computer, you can access all your Clipboard snippets in a few simple clicks.</p>
+
+        <h2>Supercharge your workflow</h2>
+        <p>We’ve got the tools to boost your productivity.</p>
+        
+
+        <h3>Create blacklists</h3>
+        <p>Ensure sensitive information never makes its way to your clipboard by excluding certain sources.</p>
+
+        <h3>Plain text snippets</h3>
+        <p>Remove unwanted formatting from copied text for a consistent look.</p>
+
+        <h3>Sneak preview</h3>
+        <p>Quick preview of all snippets on your Clipboard for easy access.</p>
+        
+      </section>
+
+      <section className='Brands'>
+        <img src={imgGoogle} alt="google logo" />
+        <img src={imgIBM} alt="ibm logo" />
+        <img src={imgMicrosoft} alt="microsoft logo" />
+        <img src={imgHP} alt="hp logo" />
+        <img src={imgVector} alt="vector graphics logo" />
       </section>
 
 
