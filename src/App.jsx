79a04871243c424e97b4f18295c7d518f5713906
ianbuen <ianbuen@gmail.com>
2023-06-06 11:13:42 +0800

    edit features section

--- src/App.jsx
+++ src/App.jsx
@@ -30,18 +30,20 @@ function App() {
         <section className='Features'>
           <h2>Keep track of your snippets</h2>
           <p>Clipboard instantly stores any item you copy in the cloud, meaning you can access your snippets immediately on all your devices. Our Mac and iOS apps will help you organize everything.</p>
-          <img src={imgPC} alt="photo of computer" />
 
           <div className='SubFeatures'>
-            <h3>Quick Search</h3>
-            <p>Easily search your snippets by content, category, web address, application, and more.</p>
-            
+            <img src={imgPC} alt="photo of computer" />
 
-            <h3>iCloud Sync</h3>
-            <p>Instantly saves and syncs snippets across all your devices.</p>
+            <div>
+              <h3>Quick Search</h3>
+              <p>Easily search your snippets by content, category, web address, application, and more.</p>
 
-            <h3>Complete History</h3>
-            <p>Retrieve any snippets from the first moment you started using the app.</p>
+              <h3>iCloud Sync</h3>
+              <p>Instantly saves and syncs snippets across all your devices.</p>
+
+              <h3>Complete History</h3>
+              <p>Retrieve any snippets from the first moment you started using the app.</p>
+            </div>
           </div>
           
           <h2>Access Clipboard anywhere</h2>
