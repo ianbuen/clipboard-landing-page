e2ebecb66a5b136d4640dbddc9964f411f83e14f
ianbuen <ianbuen@gmail.com>
2023-06-07 09:45:50 +0800

    edit vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://ianbuen.github.io/clipboard-landing-page"
 })
